453a779de2ad0b69d9299b1c1df05db180e6dc44
BT_1N3_15 <sample@bths.edu>
2024-10-25 10:57:39 -0400

    commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private int x1 = 0;
+    private int x2 = 0;
+    private int y1 = 0;
+    private int y2 = 0;
+
+
+
+
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+
+
+
+    private double distance() {
+        double d = Math.sqrt((Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+        return d;
+    }
+
+
+    private double yIntercept() {
+        double slope = slope();
+        return (y1 - (x1 * slope));
+    }
+
+
+    private double slope() {
+        return ((double)y2 - y1) / (x2 - x1);
+    }
+
+
+    private String equation() {
+        String sign = "";
+        int numerator = Math.abs(y2 - y1);
+        int denominator = Math.abs(x2 - x1);
+        String slope = numerator + "/" + denominator;
+        if (slope() < 0) {
+            sign = "-";
+        }
+        if (y2 - y1 == 0) {
+            return "y = " + yIntercept();
+        }
+        if (yIntercept() > 0) {
+            return ("y = " + sign + slope + "x " + " " + yIntercept());
+        } else if (yIntercept() == 0) {
+            return ("y = " + sign + slope + "x" + " + 0");
+        }
+        return ("y = " + sign + slope + "x" + yIntercept());
+    }
 }
+
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class LinearEquationLogic {
+import java.util.Scanner;
+
+
+public class LinearEquationLogic {
+    private Scanner scan;
+    private String firstCoordinate = "";
+    private String secondCoordinate = "";
+    private int x1 = 0;
+    private int x2 = 0;
+    private int y1 = 0;
+    private int y2 = 0;
+
+
+
+
+    public void start() {
+
+
+    }
+
+
+
+
+    private void getData() {
+        System.out.println("Welcome!");
+        System.out.print("Enter your first coordinate point: ");
+        firstCoordinate = scan.nextLine();
+        System.out.println(firstCoordinate);
+        System.out.print("Enter your second coordinate point: ");
+        secondCoordinate = scan.nextLine();
+        System.out.println(secondCoordinate);
+    }
+
+
+
+
+    private void parseData(){
+        int firstComma = firstCoordinate.indexOf(",");
+        int secondComma = secondCoordinate.indexOf(",");
+        x1 = Integer.parseInt(firstCoordinate.substring(1,firstComma));
+        x2 = Integer.parseInt(secondCoordinate.substring(1,secondComma));
+        y1 = Integer.parseInt(firstCoordinate.substring(firstComma,4));
+        y2 = Integer.parseInt(secondCoordinate.substring(secondComma,4));
+    }
+
+    LinearEquation equation1 = new LinearEquation(x1, y1, x2, y2);
 }
